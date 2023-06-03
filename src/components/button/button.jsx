a1e4a03c5ce5fe8155a759c0dc775b3c9f8bbf98
dieggspapu <alo20172@uvg.edu.gt>
2023-06-03 10:34:48 -0600

    test: button test

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types'
 import styles from './button.module.css'
 
 export default function button({
-  text, fontColor, backgroundColor, upperCase, bordered, circular,
+  text, fontColor, backgroundColor, upperCase, bordered, circular, click,
 }) {
   return (
     <div
       className={styles.Button}
       style={{ fontColor }}
     >
-      <button type="button" className={circular ? styles.buttonCircular : styles.buttonNormal} style={bordered ? { backgroundColor, color: fontColor, border: '1px solid black' } : { color: fontColor, backgroundColor }}>{upperCase === true ? text.toUpperCase() : text}</button>
+      <button type="button" onClick={click} className={circular ? styles.buttonCircular : styles.buttonNormal} style={bordered ? { backgroundColor, color: fontColor, border: '1px solid black' } : { color: fontColor, backgroundColor }}>{upperCase === true ? text.toUpperCase() : text}</button>
     </div>
   )
 }
@@ -22,4 +22,5 @@ button.propTypes = {
   upperCase: PropTypes.bool,
   bordered: PropTypes.bool,
   circular: PropTypes.bool,
+  click: PropTypes.func,
 }
---
+++ src/components/button/button.test.jsx
@@ -0,0 +1,21 @@
+import {
+  expect, it, describe, vi,
+} from 'vitest'
+import { render, screen, fireEvent } from '@testing-library/react'
+import React from 'react'
+import Button from './button'
+
+describe('Testing the button component', () => {
+  it('Can be clicked 5 times:'), () => {
+    const spy = vi.fn()
+    render(<Button click={spy} text="This is a test" />)
+    const button = screen.getByText('This is a test')
+    fireEvent.click(element)
+    fireEvent.click(element)
+    fireEvent.click(element)
+    fireEvent.click(element)
+    fireEvent.click(element)
+    expect(spy).toHaveBeenCalledTimes(5)
+    screen.debug()
+  }
+})
